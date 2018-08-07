45e1b2e3ac6c55b2e5dace2289f384a6f93f8770
lcofjp <lcofjp@163.com>
2018-08-07 18:01:53 +0800

    add drawing-shape

---
+++ drawing-shape.js
@@ -0,0 +1,61 @@
+function setDrawConfig() {
+    this._shape.strokeWidth = this._drawConfig.strokeWidth || 1;
+    this._shape.strokeColor = this._drawConfig.strokeColor || 'black';
+}
+
+// ----------------drawingRect------------------------
+function DrawingRect(event, drawConfig) {
+    this._startPoint = event.point;
+    this._drawConfig = drawConfig;
+    this._shape = new paper.Path.Rectangle(this._startPoint, this._startPoint);
+    this.setDrawConfig();
+}
+
+DrawingRect.prototype.setDrawConfig = setDrawConfig;
+
+DrawingRect.prototype.drawingDragTo = function(event) {
+    this._shape.remove();
+    this._shape = new paper.Shape.Rectangle(this._startPoint, event.point);
+    this.setDrawConfig();
+}
+DrawingRect.prototype.drawingEnd = function(event) {
+
+}
+
+
+// ----------------drawingCircle------------------------
+function DrawingCircle(event, drawConfig) {
+    this._startPoint = event.point;
+    this._drawConfig = drawConfig;
+    this._shape = new paper.Path.Circle(event.point, 0);
+    this.setDrawConfig();
+}
+
+DrawingCircle.prototype.setDrawConfig = setDrawConfig;
+
+DrawingCircle.prototype.drawingDragTo = function(event) {
+    this._shape.remove();
+    this._shape = new paper.Path.Circle(this._startPoint, event.point.subtract(this._startPoint).length);
+    this.setDrawConfig();
+}
+DrawingCircle.prototype.drawingEnd = function(event) {
+
+}
+
+// ----------------drawingLine------------------------
+function DrawingLine(event, drawConfig) {
+    this._startPoint = event.point;
+    this._drawConfig = drawConfig;
+    this._shape = new paper.Path(event.point);
+    this.setDrawConfig();
+}
+
+DrawingLine.prototype.setDrawConfig = setDrawConfig;
+
+DrawingLine.prototype.drawingDragTo = function(event) {
+    this._shape.add(event.point);
+    this._shape.smooth();
+}
+DrawingLine.prototype.drawingEnd = function(event) {
+    this._shape.simplify();
+}
--- index.js
+++ index.js
@@ -1,34 +1,59 @@
-function test() {
-    // Get a reference to the canvas object
-    var canvas = document.getElementById('canvas');
-    // Create an empty project and a view for the canvas:
-    paper.setup(canvas);
-    // Create a Paper.js Path to draw a line into it:
-    var path = new paper.Path();
-    // Give the stroke a color
-    path.strokeColor = 'black';
-    var start = new paper.Point(100, 100);
-    // Move to start and draw a line from there
-    path.moveTo(start);
-    // Note that the plus operator on Point objects does not work
-    // in JavaScript. Instead, we need to call the add() function:
-    path.lineTo(start.add([ 200, -50 ]));
-    // Draw the view now:
-    paper.view.draw();
-
-    paper.view.onMouseDown = function(event) {
-        console.log('onMouseDown: ', event);
-    }
-    paper.view.onMouseDrag = function(event) {
-        console.log('onDrag: ', event);
-    }
-
-    var path = new paper.Path.Rectangle([75, 75], [100, 100]);
-    path.strokeColor = 'black';
-    
-    paper.view.onFrame = function(event) {
-        path.rotate(1);
-    }
+var drawConfig = {
+    shape: 'line',
+    arrow: false,
+    strokeWidth: 1,
+    strokeColor: 'red'
 }
 
-test();
+function handlerInit() {
+    $('.draw-tools input').on('change', function() {
+        var i = $(this);
+        console.log(i.attr('name'), i.attr('value'))
+        var name = i.attr('name');
+        var value = i.attr('value');
+        switch(name) {
+            case 'shape-type':
+                drawConfig.shape = value;
+                if (value === 'arrow') {
+                    drawConfig.shape = 'line';
+                    drawConfig.arrow = true;
+                }
+                break;
+            case 'pen-color':
+                drawConfig.strokeColor = value;
+                break;
+            case 'pen-width':
+                drawConfig.strokeWidth = parseInt(value, 10);
+                break;
+        }
+    })
+}
+$(handlerInit);
+
+paper.setup(document.querySelector('#canvas'));
+var drawingShape;
+
+paper.view.onMouseDown = function(event) {
+    drawingShape = makeShape(event, drawConfig);
+}
+
+paper.view.onMouseDrag = function(event) {
+    drawingShape.drawingDragTo(event);
+}
+
+paper.view.onMouseUp = function(event) {
+    drawingShape.drawingEnd(event);
+}
+
+function makeShape(event, drawingConfig) {
+    var shape = drawingConfig.shape;
+    if (shape === 'rect') {
+        return new DrawingRect(event, drawConfig)
+    } else if (shape === 'circle') {
+        return new DrawingCircle(event, drawingConfig);
+    } else if (shape === 'line') {
+        return new DrawingLine(event, drawingConfig);
+    } else {
+        throw new Error('unsupported shape: ' + shape);
+    }
+}
